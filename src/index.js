109e40bd122f8f4bf9d32176d568f0d4acd7137f
Adambrb <adam.berrebi@gmail.com>
2019-10-11 10:06:30 +0200

    Ajout script js pour onglet actif et mofif langue et titre du site

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 
 import "./css/main.css";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+// onglet actif du menu
+if (window.location.href.includes("/vente/") || window.location.href.includes("/location/")) {
+  document.getElementById("venteloc").classList.add("active");
+} else if (window.location.href.includes("/diagnostics-immobiliers/")) {
+  document.getElementById("diag").classList.add("active");
+} else if (window.location.href.includes("/post/")) {
+  document.getElementById("blog").classList.add("active");
+} else if (window.location.href.includes("/contact/")) {
+  document.getElementById("contact").classList.add("active");
+}
