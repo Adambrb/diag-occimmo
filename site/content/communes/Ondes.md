ea37ec3282183398ee5ab80267628b201531eecc
Adambrb <adam.berrebi@gmail.com>
2019-11-19 15:36:39 +0100

    Modification du fond orange dans jumbotron, ajouts des communes, ajouts google maps

---
+++ site/content/communes/Ondes.md
@@ -0,0 +1,23 @@
+---
+
+---
+# Diagnostic obligatoire à Ondes
+
+Sur la commune de **Ondes**, quels sont les diagnostics immobiliers obligatoires à réaliser pour votre bien immobilier lors de la vente ou de la location ?
+
+Avant chaque transaction immobilière, que l’on soit bailleur ou vendeur, vous devez faire établir un dossier de diagnostics techniques DDT par un diagnostiqueur certifié, couvert par une assurance responsabilité civile . Ce DDT doit être produit au cours de la signature d’un contrat de bail, d’une promesse de vente ou d’un acte de vente. et même dès la mise en vente ou location de l'immeuble DPE et être en cours de validité lors de l'établissement du contrat.
+
+En contactant la société Diag OCcimmo, vous serez informés avec exactitude des diagnostics immobiliers obligatoires à réaliser de votre bien immobilier destiné à la vente ou à la location.
+
+Le diagnostiqueur vous renseignera sur la réglementation en vigueur et à votre demande, établira un devis.
+
+Les diagnostics obligatoires sur la commune de **Ondes** sont :
+
+* Le diagnostic loi Carrez
+* Le diagnostic performance énergétique
+* Le diagnostic amiante
+* Le diagnostic plomb 
+* Le diagnostic termites 
+* Le certificat d’état des risques naturels
+* Le diagnostic gaz
+* Le diagnostic électricité
---
+++ site/content/communes/test.bat
@@ -0,0 +1,30 @@
+@echo off
+set list=Aureville Aussonne Auzeville-Tolosane Balma Baziege Bazus Beauzelle Bessieres Blagnac Bonrepos-Riquet Bouloc Bruguieres Buzet-sur-Tarn Castanet-Tolosan Castelginest Castelmaurou Castelnau-dEstretefonds Cepet Colomiers Cornebarrieu Cugnaux Escalquens Fenouillet Flourens Fonbeauzard Fronton Gagnac-sur-Garonne Gargas Garidech Gragnague Gratentour Grenade Labastide-Saint-Sernin Labege Lacroix-Falgarde Lapeyrouse-Fossat Launaguet Lavalette Leguevin Lespinasse Merville Mirepoix-sur-Tarn Mondonville Montastruc-la-Conseillere Montberon Montjoire Montrabe Ondes Paulhac Pechbonnieu Pibrac Pin-Balma Plaisance-du-Touch Quint-Fonsegrives Ramonville-Saint-Agne Revel Rouffiac-Tolosan Saint-Alban Saint-Jean Saint-Jory Saint-Loup-Cammas Saint-Sauveur Seilh Toulouse Tournefeuille LUnion Vacquiers Verfeil Villaries Villaudric Villefranche-de-Lauragais Villematier Villemur-sur-Tarn Villeneuve-les-Bouloc Villeneuve-Tolosane
+set ext=.md
+(for %%a in (%list%) do (
+   echo %%a%ext% 
+   (echo ---
+    echo.
+    echo ---
+    echo # Diagnostic obligatoire à %%a
+    echo.
+    echo Sur la commune de **%%a**, quels sont les diagnostics immobiliers obligatoires à réaliser pour votre bien immobilier lors de la vente ou de la location ?
+    echo.
+    echo Avant chaque transaction immobilière, que l’on soit bailleur ou vendeur, vous devez faire établir un dossier de diagnostics techniques DDT par un diagnostiqueur certifié, couvert par une assurance responsabilité civile . Ce DDT doit être produit au cours de la signature d’un contrat de bail, d’une promesse de vente ou d’un acte de vente. et même dès la mise en vente ou location de l'immeuble DPE et être en cours de validité lors de l'établissement du contrat.
+    echo.
+    echo En contactant la société Diag OCcimmo, vous serez informés avec exactitude des diagnostics immobiliers obligatoires à réaliser de votre bien immobilier destiné à la vente ou à la location.
+    echo.
+    echo Le diagnostiqueur vous renseignera sur la réglementation en vigueur et à votre demande, établira un devis.
+    echo.
+    echo Les diagnostics obligatoires sur la commune de **%%a** sont :
+    echo.
+    echo * Le diagnostic loi Carrez
+    echo * Le diagnostic performance énergétique
+    echo * Le diagnostic amiante
+    echo * Le diagnostic plomb 
+    echo * Le diagnostic termites 
+    echo * Le certificat d’état des risques naturels
+    echo * Le diagnostic gaz
+    echo * Le diagnostic électricité
+   )> %%a%ext%
+))
